64e6cc5504be402116f897770b160c2df8ac8626
Sushma-1997 <babysushma.chitturi@slu.edu>
2022-12-09 12:47:43 -0600

    Dashboard creation, dynamic id based generation

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../helpers/database');
 
-router.get('/id/:idnumber', async function(req,res){
+router.get("/dashboard", function(req, res){
+    res.render("index/dashboard.ejs")
+})
+
+router.get('/all/:query', async function(req,res){
     try {
-        const sqlQuery = 'Select * from avengers where avenger_id='+req.params.idnumber+';';
+        const sqlQuery = 'Select * from avengers where avenger_id='+req.params.query+';';
         const rows = await pool.query(sqlQuery);
         // console.log(rows)
         res.status(200).json(rows);
--- server.js
+++ server.js
@@ -13,12 +13,16 @@ app.use(express.urlencoded({extended: false}));
 
 
 app.get("/", function(req, res){
-    res.send("landing page")
+    res.send("landing page, go to "  + "<a href='http://localhost:3000/dashboard'>Dashboard</a>?")
 })
 
+const indexRouter = require('./routes/index');
 const avengersRouter = require('./routes/avengers');
+
+app.use('/',indexRouter);
 app.use('/avengers',avengersRouter);
 
 app.listen(3000, ()=>{
-    console.log(`Running on http://localhost:${PORT}/`)
+    // console.log(`Running on http://localhost:${PORT}/`)
+    console.log(`Running on http://localhost:3000/`)
 })
