713d200799b0524739c8fc484bbf5742833d8365
sushma-1997 <sushmachitturi321@gmail.com>
2022-12-11 00:55:03 -0600

    ngrock test-1

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
+const ngrok = require("ngrok");
 
 app.get("/", function(req, res){
    res.send("app.js is loading...............");
 })
 
-app.listen(3000, () => {
-    console.log("running on http://localhost:3000/")
+app.listen(8000, () => {
+    console.log("running on http://localhost:8000/")
 })
+
+ngrok.connect({
+    proto : 'http',
+    addr : process.env.PORT,
+}, (err, url) => {
+    if (err) {
+        console.error('Error while connecting Ngrok',err);
+        return new Error('Ngrok Failed');
+    }
+});
